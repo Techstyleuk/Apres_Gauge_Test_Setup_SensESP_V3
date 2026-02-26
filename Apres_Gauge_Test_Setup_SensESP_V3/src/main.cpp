661beefba538cb7f440d1de9eb87ba749b68e6c5
Jason Greenwood <79181587+Techstyleuk@users.noreply.github.com>
2026-02-25 22:09:26 -0600

    Add files via upload

--- Apres_Gauge_Test_Setup_SensESP_V3/src/main.cpp
+++ Apres_Gauge_Test_Setup_SensESP_V3/src/main.cpp
@@ -32,21 +32,14 @@
 //
 #include <memory>
 #include "sensesp.h"
-#include "sensesp/sensors/analog_input.h"
-#include "sensesp/sensors/digital_input.h"
-#include "sensesp/sensors/sensor.h"
 #include "sensesp/signalk/signalk_output.h"
-#include "sensesp/system/lambda_consumer.h"
 #include "sensesp_app_builder.h"
 #include <Wire.h>  
 #include <Adafruit_INA219.h>
 #include "sensesp_onewire/onewire_temperature.h"
 #include <Arduino.h>
 #include "sensesp/transforms/linear.h"
-#include "sensesp/transforms/analogvoltage.h"
 #include "sensesp/transforms/curveinterpolator.h"
-#include "sensesp/transforms/voltagedivider.h"
-#include "sensesp/transforms/frequency.h"
 
 using namespace sensesp;
 using namespace sensesp::onewire;
@@ -55,35 +48,40 @@ class TempInterpreter : public CurveInterpolator {
  public:
   TempInterpreter(String config_path = "")
       : CurveInterpolator(NULL, config_path) {
-    // Populate a lookup table to translate the gauge voltage values returned by
-    // our INA219_C to Fuel Level % (Ratio)
+    // Populate a lookup table to translate the calculated Sender Resistance (R2) to the gauge Temperature (converted to Kelvin)
     clear_samples();
-    // addSample(CurveInterpolator::Sample(knownOhm, knownFuelLevel)); - these numbers need changing
-    add_sample(CurveInterpolator::Sample(1000, 0));
-    add_sample(CurveInterpolator::Sample(240, 0));
-    add_sample(CurveInterpolator::Sample(218, .1));
-    add_sample(CurveInterpolator::Sample(198, .2));
-    add_sample(CurveInterpolator::Sample(177, .3));
-    add_sample(CurveInterpolator::Sample(156, .4));
-    add_sample(CurveInterpolator::Sample(135, .5));
-    add_sample(CurveInterpolator::Sample(114, .6));
-    add_sample(CurveInterpolator::Sample(93, .7));
-    add_sample(CurveInterpolator::Sample(72, .8)); 
-    add_sample(CurveInterpolator::Sample(51, .9));
-    add_sample(CurveInterpolator::Sample(30, 1)); 
-    add_sample(CurveInterpolator::Sample(0, 1)); 
+    // addSample(CurveInterpolator::Sample(knownOhm, knownTemperature-K)); - these numbers need changing
+    add_sample(CurveInterpolator::Sample(1000, 200));
+    add_sample(CurveInterpolator::Sample(147, 280));
+    add_sample(CurveInterpolator::Sample(141, 290));
+    add_sample(CurveInterpolator::Sample(135, 300));
+    add_sample(CurveInterpolator::Sample(129, 310));
+    add_sample(CurveInterpolator::Sample(123, 320));
+    add_sample(CurveInterpolator::Sample(115, 329));
+    add_sample(CurveInterpolator::Sample(105, 340));
+    add_sample(CurveInterpolator::Sample(99, 346));
+    add_sample(CurveInterpolator::Sample(0, 400)); 
   }
 };
-////////////////////INA219
+////////////INA219
   Adafruit_INA219 ina219_tg;
  
-  const float Rgauge = 25; //this is the measured resistance of the gauge
+  const float Rgauge = 189; //this is the measured resistance of the gauge
 
-  float read_TG_shuntvoltage_callback() { return (ina219_tg.getShuntVoltage_mV() / 1000);} //drop across the shunt - this is for debug 
-  float read_TG_busvoltage_callback() { return (ina219_tg.getBusVoltage_V());} //downstream voltage to be supplied to the consumer - this is for debug
-  float read_TG_loadvoltage_callback() { return (ina219_tg.getBusVoltage_V() + (ina219_tg.getShuntVoltage_mV() / 1000));} //loadvoltage is the battery or supply voltage
-  float read_TG_r2resistance_callback() { return (ina219_tg.getBusVoltage_V() + (ina219_tg.getShuntVoltage_mV() / 1000));} //Calculation not complete yet, make this calculate R2 manually
-  //////////////////////INA219 end
+  float read_TG_shuntvoltage_callback() { return (ina219_tg.getShuntVoltage_mV() / 1000);} //drop across the shunt (which is the gauge: Vin - Vout) - this is for debug 
+  float read_TG_busvoltage_callback() { return (ina219_tg.getBusVoltage_V());} //downstream voltage to be supplied to the consumer (Vout) - this is for debug
+  float read_TG_loadvoltage_callback() { return (ina219_tg.getBusVoltage_V() + (ina219_tg.getShuntVoltage_mV() / 1000));} //loadvoltage is the battery or supply voltage (Vin)
+  float read_TG_current_callback() { return (ina219_tg.getCurrent_mA() / 1000);} //Current mA
+  float read_TG_r2resistance_callback() { 
+    float I_mA = ina219_tg.getCurrent_mA();
+    if (I_mA <= 0.0001f) {
+        return NAN;
+    }
+    float V_bus   = ina219_tg.getBusVoltage_V();
+    float R_total = V_bus / (I_mA / 1000.0f);
+    return R_total - Rgauge;
+}
+////////////INA219 end
 
 // The setup function performs one-time application initialization.
 void setup() {
@@ -101,14 +99,14 @@ void setup() {
                     //->set_wifi_client("My WiFi SSID", "my_wifi_password")
                     //->set_wifi_access_point("My AP SSID", "my_ap_password")
                     //->set_sk_server("192.168.10.3", 80)
+                    ->enable_ota("thisisfine")
                     ->get_app();
 
- // 3) Direct copy from old Main.ccp - Start 
-/// 1-Wire Temp Sensors (amended for SensESP version 3)
-
+/// 1-Wire Temp Sensor
   DallasTemperatureSensors* dts = new DallasTemperatureSensors(25); //digital 2
 
- // A Battery Temperature - electrical.batteries.A.temperature
+ // reference Temperature - this is for demonstration purposes to show how to use a 1-Wire sensor as a reference temperature for the gauge sender.
+ // This is not needed for the gauge sender to work. You can remove this and the OneWireTemperature class if you don't need a reference temperature sensor.
   auto* ref_temp =
       new OneWireTemperature(dts, 1000, "/Reference Temperature/oneWire");
 
@@ -126,7 +124,7 @@ void setup() {
       ->set_sort_order(200);
 
   auto ref_temp_sk_output = new SKOutputFloat(
-      "environment.reference.temperature", "/Reference Temperature/skPath");
+      "environment.reference.temperature", "/Reference Temperature/skPath", "K");
 
   ConfigItem(ref_temp_sk_output)
       ->set_title("Reference Temperature Signal K Path")
@@ -136,12 +134,10 @@ void setup() {
   ref_temp->connect_to(ref_temp_calibration)
       ->connect_to(ref_temp_sk_output);
 
-///////////////////////////INA219 start
-  ina219_tg.begin();  // Initialize second board with the address 0x45 ----> A1 and A0 soldered = 0x45
+/////INA219 start
+  ina219_tg.begin();  // Initialize first board (default address 0x40)
 
-//ina219_D
-  //// Fuel Sender Config ////
-
+  //// Temperature Sender Config ////
   auto* ina219_TG_shuntvoltage = 
       new RepeatSensor<float>(1000, read_TG_shuntvoltage_callback); //drop across the shunt
 
@@ -151,24 +147,34 @@ void setup() {
   auto* ina219_TG_loadvoltage = 
       new RepeatSensor<float>(1000, read_TG_loadvoltage_callback);   //battery voltage
 
+    auto* ina219_TG_current = 
+      new RepeatSensor<float>(1000, read_TG_current_callback);   //battery voltage
+
   auto* ina219_TG_r2resistance = 
       new RepeatSensor<float>(1000, read_TG_r2resistance_callback);   //r2 resistance      
 
   // Send the temperature to the Signal K server as a Float
-
-  ina219_TG_loadvoltage->connect_to(new SKOutputFloat("engine.coolant.Vin.voltage")); 
-
-  ina219_TG_shuntvoltage->connect_to(new SKOutputFloat("engine.coolant.differential.voltage")); 
-
-  ina219_TG_busvoltage->connect_to(new SKOutputFloat("engine.coolant.Vout.voltage")); 
-
-  ina219_TG_r2resistance->connect_to(new SKOutputFloat("engine.coolant.R2.resistance")); 
-
+  ina219_TG_loadvoltage
+  ->connect_to(new SKOutputFloat("engine.coolant.Vin.voltage",
+                                 "/engine/coolant/Vin", "V"));
+ina219_TG_shuntvoltage
+  ->connect_to(new SKOutputFloat("engine.coolant.differential.voltage",
+                                 "/engine/coolant/diffV", "V"));
+ina219_TG_busvoltage
+  ->connect_to(new SKOutputFloat("engine.coolant.Vout.voltage",
+                                 "/engine/coolant/Vout", "V"));
+ina219_TG_current
+  ->connect_to(new SKOutputFloat("engine.coolant.current",
+                                 "/engine/coolant/current", "A"));
 ina219_TG_r2resistance
-    ->connect_to(new TempInterpreter("/engine/coolant/curve"))
-    ->connect_to(new Linear(1.0, 0.0, "/engine/coolant/calibrate"))
-    ->connect_to(new SKOutputFloat("engine.coolant.temperature",
-                                   "/engine/coolant/sk_path"));
+  ->connect_to(new SKOutputFloat("engine.coolant.R2.resistance",
+                                 "/engine/coolant/R2", "Ohm"));
+
+  ina219_TG_r2resistance  //take the R2 resistance
+    ->connect_to(new TempInterpreter("/engine/coolant/curve"))   //Convert to temperature using the curve interpolator
+    ->connect_to(new Linear(1.0, 0.0, "/engine/coolant/calibrate"))  //Calibrate the temperature if needed using a linear transform
+    ->connect_to(new SKOutputFloat("engine.coolant.temperature",   //send the temperature to the Signal K server as a Float
+                                   "/engine/coolant/sk_path", "K"));
 
   // To avoid garbage collecting all shared pointers created in setup(),
   // loop from here.
